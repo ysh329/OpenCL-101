bd7c20b7c8ab027facfa7d7ef59961ed6f80e6a4
ysh329@sina.com <ysh329@sina.com>
2017-10-11 02:29:48 +0000

    add

---
+++ common/benchmark.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+double get_gflops(double sum_duration, int run_num, int size) {
+    double gflops = 2.0 * size / (double)sum_durationsize / (double)run_num * 1.0e-9;
+    return gflops;
+}
+
+double get_gbps(double sum_duration, int run_num, int size, int elem_size) {
+    double gbps = 2.0 * size * elem_size / (double)(1024 * 1024 * 1024) / sum_duration / (double)run_num;
+    return gbps;
+}
