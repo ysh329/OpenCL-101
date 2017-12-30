189c06427dd86dea352be4f96aed1074e8ddc4aa
ysh329 <ysh329@sina.com>
2017-12-30 08:10:01 +0000

    tried int2 index without performance improve

--- mat-mult/gemm_interleave_trans.c
+++ mat-mult/gemm_interleave_trans.c
@@ -55,17 +55,12 @@ __kernel void gemm_interleaved_transposed_vec4(const int aI_height, // height of
                                                __global const CL_INPUT_TYPE *aI,
                                                __global const CL_INPUT_TYPE *bT,
                                                __global       CL_INPUT_TYPE *c) {
-   // const int col = get_global_id(0); // col of bT: [0, n/4) <==> [0, bT_height)
-   // const int row = get_global_id(1); // row of aI: [0, m/4) <==> [0, aI_height)
+    //const int col = get_global_id(0); // col of bT: [0, n/4) <==> [0, bT_height)
+    //const int row = get_global_id(1); // row of aI: [0, m/4) <==> [0, aI_height)
     const int col = get_group_id(1) * 4 + get_local_id(1);
     const int row = get_group_id(0) * 4 + get_local_id(0);
 
-    CL_ELEM_TYPE c00 = 0.0f,
-                 c10 = 0.0f,
-                 c20 = 0.0f,
-                 c30 = 0.0f;
-
-    for (int p = 0; p < aI_width; p += 12) {
+    for (int p = 0; p < aI_width; p += 8) {
         CL_ELEM_TYPE
         aa = vload4(0, aI + row * aI_width + p),
         bb = vload4(0, bT + col * aI_width + p);
@@ -75,7 +70,6 @@ __kernel void gemm_interleaved_transposed_vec4(const int aI_height, // height of
         c20 += (CL_ELEM_TYPE)aa.s2 * bb;
         c30 += (CL_ELEM_TYPE)aa.s3 * bb;
 
-        
         aa = vload4(0, aI + row * aI_width + p+4);
         bb = vload4(0, bT + col * aI_width + p+4);
 
@@ -83,20 +77,10 @@ __kernel void gemm_interleaved_transposed_vec4(const int aI_height, // height of
         c10 += (CL_ELEM_TYPE)aa.s1 * bb; 
         c20 += (CL_ELEM_TYPE)aa.s2 * bb;
         c30 += (CL_ELEM_TYPE)aa.s3 * bb;
-
-
-        aa = vload4(0, aI + row * aI_width + p+8);
-        bb = vload4(0, bT + col * aI_width + p+8);
-
-        c00 += (CL_ELEM_TYPE)aa.s0 * bb;
-        c10 += (CL_ELEM_TYPE)aa.s1 * bb; 
-        c20 += (CL_ELEM_TYPE)aa.s2 * bb;
-        c30 += (CL_ELEM_TYPE)aa.s3 * bb;       
     }
 
     vstore4(c00, 0, c+(row*4  ) * (bT_height*4) + (col*4));
     vstore4(c10, 0, c+(row*4+1) * (bT_height*4) + (col*4));
     vstore4(c20, 0, c+(row*4+2) * (bT_height*4) + (col*4));
     vstore4(c30, 0, c+(row*4+3) * (bT_height*4) + (col*4));
-
 }
